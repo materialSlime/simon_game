ff9e2d09b923f2386855a2036c2fd4d881c2cc7f
turing_tested <materialSlime@users.noreply.github.com>
2024-04-06 20:52:15 +0530

    last commit

--- index.js
+++ index.js
@@ -1,88 +1,69 @@
+let buttons = ['green', 'red', 'yellow', 'blue'];
 let flashSequence = [];
+let playerSequence = [];
 let level = 0;
 let isGameOn = false;
+let gameStatus = $('.game-status');
 
 function newSequence() {
-    randomBox = Math.floor(Math.random() * 4)
-    switch (randomBox) {
-        case 0:
-            flashSequence.push('green')
-            break;
-        case 1:
-            flashSequence.push('red')
-            break;
-        case 2:
-            flashSequence.push('yellow')
-            break;
-        case 3:
-            flashSequence.push('blue')
-            break;
-        default:
-            console.log("Error in adding new sequence", randomBox);
-            break;
-
-    }
-    lastItem = flashSequence[flashSequence.length - 1]
-    playSfx(lastItem);
-    flash(lastItem)
-    level++;
-    $('.game-status').text(`Level ${level}`);
+    lastInSq = Math.floor(Math.random() * 4);
+    flashSequence.push(buttons[lastInSq]);
+    gameStatus.html(`Level ${level}`);
+    console.log(flashSequence);
+    playSfx(buttons[lastInSq]);
+    flash(buttons[lastInSq]);
 }
-function playSfx(button) {
-    let sxf = new Audio(`./sounds/${button}.mp3`);
-    sxf.play();
-}
-function flash(element) {
-    a = $(`.${element}  div`)
-    a.addClass('pulse');
-    setTimeout(() => a.removeClass('pulse'), "200");
-}
+function gameEnd() {
+    playerSequence = [];
+    flashSequence = [];
+    isGameOn = false;
+    gameStatus.html(`Game Over!!<br> Score:${level}`);
+    playSfx('wrong');
+    level = 0;
 
-
-function sequenceCheck(playerChoice) {
-    for (n = 0; n < flashSequence.length; n++) {
-        if (playerChoice == flashSequence[n]) {
-            continue;
+}
+function checkAnswer() {
+    let initialAr = flashSequence.length;
+    let isEqual;
+    if (initialAr === playerSequence.length) {
+        if (flashSequence.every((value, index) => value === playerSequence[index])) {
+            setTimeout(() => {
+                newSequence();
+            }, 1000)
+            level++;
+            playerSequence = [];
         }
         else {
-            playSfx("wrong");
-            return false;
+            console.log("SeqErr: No Match");
+            gameEnd();
         }
     }
-    console.log("Hello There")
-    return true;
+    else {
+        console.log("SeqErr: Wrong Size");
+    }
 }
 
 
-
-$('body').on('keydown mousedown touchdown', function () {
+$('body').on("mousedown keydown touchdown", function () {
     if (!isGameOn) {
-        isGameOn = true;
         newSequence();
+        isGameOn = true;
     }
 });
 
 $('.game-box').on('click', function () {
     playerChoice = $(this).attr('class').split(' ')[1];
-    sequenceCheck(playerChoice);
+    playerSequence.push(playerChoice);
+    console.log(playerSequence)
+    checkAnswer();
 });
-
-/*
-Any Button Press
-New Sequence 
-
-plaver clicks
-checked to order
-
-
-*/
 
 
 
 $('.game-box').on({
     "mousedown touchstart": function () {
+        playSfx($(this).attr('class').split(' ')[1]);
         $(this).addClass("bounce");
-        playSfx(this.classList[1]);
     },
     "mouseup  touchend": function () {
         $(this).removeClass("bounce");
@@ -91,3 +72,15 @@ $('.game-box').on({
         $(this).removeClass("bounce");
     }
 });
+
+function playSfx(fileName) {
+    sfx = new Audio(`./sounds/${fileName}.mp3`);
+    sfx.play();
+}
+function flash(box) {
+    flashButton = "." + box + " div"
+    $(flashButton).addClass('flash');
+    setTimeout(function () {
+        $(flashButton).removeClass('flash');
+    }, 300);
+}
